5410426ab209ff1d796386d7addeab3c21dde847
4j4yk <git@ajayk.me>
2026-09-15 17:07:50 -0400

    Refresh production cache after AEM sync

--- cdn/aem-proxy-worker.js
+++ cdn/aem-proxy-worker.js
@@ -1,5 +1,5 @@
 const ORIGIN_HOSTNAME = 'main--strawberry-aviation-aem--4j4yk.aem.live';
-const CACHE_VERSION = 'aem-experience-reset-2026-09-15';
+const CACHE_VERSION = 'aem-experience-reset-2026-09-15-2';
 
 function extension(pathname) {
   const basename = pathname.split('/').pop();
