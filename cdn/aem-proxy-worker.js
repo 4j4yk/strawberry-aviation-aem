b5956193d1770029faa21e5d68b1e45c5fdaf442
4j4yk <git@ajayk.me>
2026-09-21 14:42:33 -0400

    Refresh guarded assistant assets

--- cdn/aem-proxy-worker.js
+++ cdn/aem-proxy-worker.js
@@ -1,5 +1,5 @@
 const ORIGIN_HOSTNAME = 'main--strawberry-aviation-aem--4j4yk.aem.live';
-const CACHE_VERSION = 'aem-grounded-assistant-2026-09-21-1';
+const CACHE_VERSION = 'aem-assistant-guardrails-2026-09-21-1';
 
 function extension(pathname) {
   const basename = pathname.split('/').pop();
